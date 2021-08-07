e55e0e229aa8050874364cb54abf85b188c08a66
Miguel Mendoza <miguel@lisainsurtech.com>
2021-08-07 01:31:02 -0400

    Add token in post user response. Remove token validation in post user

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,159 +1,157 @@
-const bcrypt = require('bcryptjs');
-const User = require('../models/user');
-const { generateJWT } = require('../helpers/jwt');
+const bcrypt = require("bcryptjs");
+const User = require("../models/user");
+const { generateJWT } = require("../helpers/jwt");
 
 // ===========================================
 // ============== GET users ==============
 // ===========================================
 const getUsers = async (req, res) => {
+  const from = Number(req.query.from) || 0;
+  const limit = Number(req.query.total) || 5;
 
-    const from = Number(req.query.from) || 0;
-    const limit = Number(req.query.total) || 5;
+  try {
+    const [users, total] = await Promise.all([
+      // Se usa el promise.all para que empiece a resolver las promesas de manera simultanea
+      User.find().skip(from).limit(limit),
+      User.countDocuments(),
+    ]);
 
-    try {
-        const [users, total] = await Promise.all([ // Se usa el promise.all para que empiece a resolver las promesas de manera simultanea
-            User.find().skip(from).limit(limit),
-            User.countDocuments()
-        ]);
-
-        res.json({
-            ok: true,
-            users,
-            total
-        })
-    } catch (error) {
-        return res.status(500).json({
-            ok: false,
-            msg: "Ha ocurrido un error inesperado"
-        })
-    }
-}
+    res.json({
+      ok: true,
+      users,
+      total,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      ok: false,
+      msg: "Ha ocurrido un error inesperado",
+    });
+  }
+};
 
 // ===========================================
 // ============== POST new user ==============
 // ===========================================
 const postUser = async (req, res) => {
-    var body = req.body;
+  var body = req.body;
 
-    const existEmail = await User.findOne({email: body.email});
+  const existEmail = await User.findOne({ email: body.email });
 
-    if( existEmail ) {
-        return res.status(400).json({
-            ok: false,
-            msg: 'El correo ya existe'
-        });
+  if (existEmail) {
+    return res.status(400).json({
+      ok: false,
+      msg: "El correo ya existe",
+    });
+  }
+
+  var user = new User({
+    name: body.name,
+    email: body.email,
+    password: bcrypt.hashSync(body.password, 10),
+    image: body.image,
+    role: body.role,
+  });
+
+  user.save(async (err, userSaved) => {
+    if (err) {
+      return res.status(400).json({
+        ok: false,
+        message: "Error creando usuario",
+        errors: err,
+      });
     }
 
-    var user = new User({
-        name: body.name,
-        email: body.email,
-        password: bcrypt.hashSync(body.password, 10),
-        image: body.image,
-        role: body.role
+    const token = await generateJWT(userSaved.id);
+
+    res.status(201).json({
+      ok: true,
+      token,
+      user: userSaved,
     });
-
-    user.save(async (err, userSaved) => {
-        if(err) {
-            return res.status(400).json({
-                ok: false,
-                message: 'Error creando usuario',
-                errors: err
-            });
-        }
-
-        const token = await generateJWT(userSaved.id);
-
-        res.status(201).json({
-            ok: true,
-            user: userSaved,
-            token
-        });
-    });
-}
+  });
+};
 
 // ===========================================
 // ============== PUT edit user ==============
 // ===========================================
 const putUser = (req, res) => {
-    const id = req.params.id;
-    const body = req.body;
+  const id = req.params.id;
+  const body = req.body;
 
-    User.findById(id, async (err, user) => {
-        if(err) {
-            return res.status(500).json({
-                ok: false,
-                message: 'Error encontrando usuario!',
-                errors: err
-            });
-        }
+  User.findById(id, async (err, user) => {
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        message: "Error encontrando usuario!",
+        errors: err,
+      });
+    }
 
-        if(!user) {
-            return res.status(400).json({
-                ok: false,
-                message: 'Error encontrando usuario',
-                errors: {message: 'El usuario con el ID: ' + id + ' no existe'}
-            });
-        }
+    if (!user) {
+      return res.status(400).json({
+        ok: false,
+        message: "Error encontrando usuario",
+        errors: { message: "El usuario con el ID: " + id + " no existe" },
+      });
+    }
 
-        const { password, google, email, ...fields } = body;
+    const { password, google, email, ...fields } = body;
 
-        const updatedUser = await User.findByIdAndUpdate(id, fields, { new: true });
+    const updatedUser = await User.findByIdAndUpdate(id, fields, { new: true });
 
-        if(user.email !== email) {
-            const emailExists = await User.findOne({ email });
-            if(emailExists) {
-                res.status(400).json({
-                    ok: false,
-                    msg: 'Ya existe un usuario con ese correo'
-                });
-            }
-        }
-
-        fields.email = email;
-
-        res.json({
-            ok: true,
-            user: updatedUser
+    if (user.email !== email) {
+      const emailExists = await User.findOne({ email });
+      if (emailExists) {
+        res.status(400).json({
+          ok: false,
+          msg: "Ya existe un usuario con ese correo",
         });
+      }
+    }
 
+    fields.email = email;
+
+    res.json({
+      ok: true,
+      user: updatedUser,
     });
-}
+  });
+};
 
 // ===========================================
 // ============== DELETE User ================
 // ===========================================
 const deleteUser = async (req, res) => {
-    const id = req.params.id;
+  const id = req.params.id;
 
-    try {
-        const dbUser = await User.findById(id);
+  try {
+    const dbUser = await User.findById(id);
 
-        if(!dbUser) {
-            return res.status(400).json({
-                ok: false,
-                msg: 'No se ha encontrado ningun usuario'
-            })
-        }
-
-        await User.findByIdAndDelete(id);
-
-        res.json({
-            ok: true,
-            msg: 'Usuario eliminado'
-        })
-
-    } catch(err) {
-        console.log(err);
-        return res.status(500).json({
-            ok: false,
-            msg: 'Ha ocurrido un error inesperado!'
-        });
+    if (!dbUser) {
+      return res.status(400).json({
+        ok: false,
+        msg: "No se ha encontrado ningun usuario",
+      });
     }
-}
+
+    await User.findByIdAndDelete(id);
+
+    res.json({
+      ok: true,
+      msg: "Usuario eliminado",
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      ok: false,
+      msg: "Ha ocurrido un error inesperado!",
+    });
+  }
+};
 
 module.exports = {
-    getUsers,
-    postUser,
-    putUser,
-    deleteUser
-}
+  getUsers,
+  postUser,
+  putUser,
+  deleteUser,
+};
--- routes/user.js
+++ routes/user.js
@@ -1,33 +1,45 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
 
-const { check } = require('express-validator');
-const { validateFields } = require('../middlewares/fieldsValidator');
-const { validateJWT } = require('../middlewares/auth');
+const { check } = require("express-validator");
+const { validateFields } = require("../middlewares/fieldsValidator");
+const { validateJWT } = require("../middlewares/auth");
 
-const { getUsers, postUser, putUser, deleteUser } = require('../controllers/users.controller');
+const {
+  getUsers,
+  postUser,
+  putUser,
+  deleteUser,
+} = require("../controllers/users.controller");
 
 // GET Users
-router.get('/', validateJWT, getUsers);
+router.get("/", validateJWT, getUsers);
 
 // POST user
-router.post('/', [
-    validateJWT,
-    check('name', 'El campo NOMBRE es requerido').notEmpty(),
-    check('password', 'El campo PASSWORD es requerido').notEmpty(),
-    check('email', 'El campo EMAIL es requerido').isEmail(),
-    validateFields
-], postUser);
+router.post(
+  "/",
+  [
+    check("name", "El campo NOMBRE es requerido").notEmpty(),
+    check("password", "El campo PASSWORD es requerido").notEmpty(),
+    check("email", "El campo EMAIL es requerido").isEmail(),
+    validateFields,
+  ],
+  postUser
+);
 
 // PUT user
-router.put('/:id', [
+router.put(
+  "/:id",
+  [
     validateJWT,
-    check('name', 'El campo NOMBRE es requerido').notEmpty(),
-    check('email', 'El campo EMAIL es requerido').isEmail(),
-    validateFields
-], putUser);
+    check("name", "El campo NOMBRE es requerido").notEmpty(),
+    check("email", "El campo EMAIL es requerido").isEmail(),
+    validateFields,
+  ],
+  putUser
+);
 
 // DELETE user
-router.delete('/:id', validateJWT, deleteUser);
+router.delete("/:id", validateJWT, deleteUser);
 
 module.exports = router;
